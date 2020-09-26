e483c2231fc324ea9643b5dccc4288f98e96c419
Tymur Foshch <antariuz@gmail.com>
2020-09-26 14:58:05 +0300

    First commit

--- src/mysql/person/Main.java
+++ src/mysql/person/Main.java
@@ -3,6 +3,12 @@ package mysql.person;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Person person = new Person.Builder()
+            .withID("123")
+            .withName("Artur")
+            .withSurname("King")
+            .withAge(66)
+            .build();
+        System.out.println("person" + person);
     }
 }
---
+++ src/mysql/person/Person.java
@@ -0,0 +1,85 @@
+package mysql.person;
+
+public class Person {
+
+    private String id;
+    private String name;
+    private String surname;
+    private Integer age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public static class Builder {
+        private Person newPerson;
+
+        public Builder() {
+            newPerson = new Person();
+        }
+
+        public Builder withID(String id) {
+            newPerson.id = id;
+            return this;
+        }
+
+        public Builder withName(String name) {
+            newPerson.name = name;
+            return this;
+        }
+
+        public Builder withSurname(String surname) {
+            newPerson.surname = surname;
+            return this;
+        }
+
+        public Builder withAge(Integer age) {
+            newPerson.age = age;
+            return this;
+        }
+
+        public Person build() {
+            return newPerson;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return " = {" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+}
